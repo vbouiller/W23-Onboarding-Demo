e4420e2733b221d0381090f855f918544fd8e5df
Valentin Bouiller <31282661+vbouiller@users.noreply.github.com>
2023-03-28 21:01:39 +0200

    Feat: demo init

--- terraform.tf
+++ terraform.tf
@@ -1,10 +1,10 @@
 terraform {
 /*
   cloud {
-    organization = "organization-name"
+    organization = "vbouiller-org"
 
     workspaces {
-      name = "learn-terraform-cloud"
+      name = ""
     }
   }
 */
